6422bd2ce22b7823f4eba3952e0c6239fccc9a4d
Jan Engelhardt <jengelh@inai.de>
2014-11-08 21:40:49 +0100

    prefs: use separate directory for config
    Do not overwrite an existing Chromium configuration.

--- chrome/common/chrome_paths_linux.cc
+++ chrome/common/chrome_paths_linux.cc
@@ -94,7 +94,7 @@ bool GetDefaultUserDataDirectory(base::FilePath* result) {
 #elif BUILDFLAG(GOOGLE_CHROME_BRANDING)
   std::string data_dir_basename = "google-chrome";
 #else
-  std::string data_dir_basename = "chromium";
+  std::string data_dir_basename = "iridium";
 #endif
   *result = config_dir.Append(data_dir_basename + GetChannelSuffixForDataDir());
   return true;

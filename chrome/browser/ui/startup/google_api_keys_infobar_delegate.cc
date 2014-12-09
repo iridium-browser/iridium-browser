c6e296664b0cf4a167a981d666ad7f274be5fc9d
Jan Engelhardt <jengelh@inai.de>
2014-12-09 17:19:41 +0100

    browser-ui: disable warning about missing API keys
    We do not want to use the services anyway ;)

--- chrome/browser/ui/startup/google_api_keys_infobar_delegate.cc
+++ chrome/browser/ui/startup/google_api_keys_infobar_delegate.cc
@@ -17,6 +17,7 @@
 // static
 void GoogleApiKeysInfoBarDelegate::Create(
     infobars::ContentInfoBarManager* infobar_manager) {
+  return;
   infobar_manager->AddInfoBar(
       CreateConfirmInfoBar(std::unique_ptr<ConfirmInfoBarDelegate>(
           new GoogleApiKeysInfoBarDelegate())));

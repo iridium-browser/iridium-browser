a53211f34054099c4a136db63f715472e677c6bc
Jan Engelhardt <jengelh@inai.de>
2015-01-26 13:57:44 +0100

    src: use cached copy of safebrowsing lists at iridiumbrowser.de
    To avoid contacing Google, let the UA use safebrowsing lists hosted at
    iridiumbrowser. The Iridium project re-gathers these lists on a
    regular basis.

--- components/safe_browsing/content/browser/client_side_model_loader.cc
+++ components/safe_browsing/content/browser/client_side_model_loader.cc
@@ -61,7 +61,7 @@ std::string ReadFileIntoString(base::FilePath path) {
 const size_t ModelLoader::kMaxModelSizeBytes = 150 * 1024;
 const int ModelLoader::kClientModelFetchIntervalMs = 3600 * 1000;
 const char ModelLoader::kClientModelUrlPrefix[] =
-    "https://ssl.gstatic.com/safebrowsing/csd/";
+    "https://cache.iridiumbrowser.de/";
 const char ModelLoader::kClientModelNamePattern[] =
     "client_model_v5%s_variation_%d.pb";
 #if !BUILDFLAG(FULL_SAFE_BROWSING)

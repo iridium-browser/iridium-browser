ffc6cb40c518a35bc35668df8109ea2e7c86e30c
Jan Engelhardt <jengelh@inai.de>
2015-02-13 00:59:04 +0100

    updater: disable auto-update
    As per http://www.chromium.org/administrators/turning-off-auto-updates ,
    the auto update function is decidedly disabled on Linux, i.e.
    the following patch is for Windows and MacOS.
    
    For Windows, all we need is to build without -DGOOGLE_CHROME_BUILD (cf.
    chrome/installer/util/google_update_settings.cc), which may already be
    the case anyway, since we are based off Chromium, not Chrome.

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -671,7 +671,7 @@ static base::mac::ScopedObjCClassSwizzler* g_swizzle_imk_input_session;
   CFStringRef checkInterval = CFSTR("checkInterval");
   CFPropertyListRef plist = CFPreferencesCopyAppValue(checkInterval, app);
   if (!plist) {
-    const float fiveHoursInSeconds = 5.0 * 60.0 * 60.0;
+    const float fiveHoursInSeconds = 0.0;
     NSNumber* value = [NSNumber numberWithFloat:fiveHoursInSeconds];
     CFPreferencesSetAppValue(checkInterval, value, app);
     CFPreferencesAppSynchronize(app);

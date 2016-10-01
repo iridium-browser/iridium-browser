f92a5b7856dcdbb8acabadffc1cf7ecac9d0a246
Jan Engelhardt <jengelh@inai.de>
2016-10-01 11:23:40 +0200

    first_run: barf if metrics_reporting is activated
    This patch is for debugging purposes and is meant for discovering
    whether  despite our patching efforts  metrics_reporting is still
    enabled somehow, and if so, say so on stderr.
    
    Result: As of October 2014, metrics_reporting seems successfully
    disabled, as no message is emitted.

--- chrome/browser/first_run/first_run_internal_posix.cc
+++ chrome/browser/first_run/first_run_internal_posix.cc
@@ -23,6 +23,7 @@
 #include "components/metrics/metrics_reporting_default_state.h"
 #include "components/prefs/pref_service.h"
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
+#include <cstdio>
 
 namespace first_run {
 
@@ -77,6 +78,9 @@ bool ShouldShowFirstRunDialog() {
   // For real first runs, Mac and Desktop Linux initialize the default metrics
   // reporting state when the first run dialog is shown.
   bool is_opt_in = first_run::IsMetricsReportingOptIn();
+      if (is_opt_in) {
+        fprintf(stderr, "*** metrics_reporting = 1\n");
+      }
   metrics::RecordMetricsReportingDefaultState(
       g_browser_process->local_state(),
       is_opt_in ? metrics::EnableMetricsDefault::OPT_IN

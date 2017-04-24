49bfb032ef8ea9548de71f6082404fe9d5b2736b
Jan Engelhardt <jengelh@inai.de>
2017-04-24 16:11:53 +0200

    prefs: disable scrolling animations
    One hardware input should correspond to one screen change.
    Everything else is a waste of time for the user.

--- ui/gfx/animation/animation_linux.cc
+++ ui/gfx/animation/animation_linux.cc
@@ -13,6 +13,7 @@ namespace {
 // GTK only has a global setting for whether animations should be enabled.  So
 // use it for all of the specific settings that Chrome needs.
 bool AnimationsEnabled() {
+	return false;
   auto* provider = AnimationSettingsProviderLinux::GetInstance();
   return !provider || provider->AnimationsEnabled();
 }

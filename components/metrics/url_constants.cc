165c942e9b29acc40d1308c17eccfda1e384286e
Jan Engelhardt <jengelh@inai.de>
2019-09-30 09:37:51 +0200

    all: add trk: prefixes to possibly evil connections
    Prefix URLs to Google services with trk: so that whenever something
    tries to load them, the developer will be informed via printf and
    dialog (extra info bar between URLbar and content window) about this.
    
    If you see such dialog, we know that (a) either the URL needs to be
    whitelisted, or (b) the feature that triggered it needs to be disabled
    by default.

--- components/metrics/url_constants.cc
+++ components/metrics/url_constants.cc
@@ -7,7 +7,7 @@
 namespace metrics {
 
 const char kNewMetricsServerUrl[] =
-    "https://clientservices.googleapis.com/uma/v2";
+    "trk:265:https://clientservices.googleapis.com/uma/v2";
 
 const char kNewMetricsServerUrlInsecure[] =
     "http://clientservices.googleapis.com/uma/v2";

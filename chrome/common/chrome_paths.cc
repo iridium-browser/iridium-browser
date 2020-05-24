1ad3fa3740dcfc9743bcf9cea0561347f44d5053
Jan Engelhardt <jengelh@inai.de>
2020-05-24 08:24:42 +0200

    prefs: use system-provided preference skeleton
    This patch makes it possible for distributions to have a global
    default preferences file in /etc/iridium-browser/preferences that get
    copied over to ~ when the user starts the program for the first time.

--- chrome/common/chrome_paths.cc
+++ chrome/common/chrome_paths.cc
@@ -424,9 +424,9 @@ bool PathProvider(int key, base::FilePath* result) {
 #if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_OPENBSD)
     case chrome::DIR_POLICY_FILES: {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-      cur = base::FilePath(FILE_PATH_LITERAL("/etc/opt/chrome/policies"));
+      cur = base::FilePath(FILE_PATH_LITERAL("/etc/opt/iridium-browser/policies"));
 #else
-      cur = base::FilePath(FILE_PATH_LITERAL("/etc/chromium/policies"));
+      cur = base::FilePath(FILE_PATH_LITERAL("/etc/iridium-browser/policies"));
 #endif
       break;
     }
@@ -504,7 +504,7 @@ bool PathProvider(int key, base::FilePath* result) {
           "/etc/opt/chrome/native-messaging-hosts"));
 #else
       cur = base::FilePath(FILE_PATH_LITERAL(
-          "/etc/chromium/native-messaging-hosts"));
+          "/etc/iridium-browser/native-messaging-hosts"));
 #endif
 #endif  // !BUILDFLAG(IS_MAC)
       break;

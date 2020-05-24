21d65dddc0707840119a2f37f9436d60403a6ca3
Jan Engelhardt <jengelh@inai.de>
2020-05-24 08:24:42 +0200

    prefs: use system-provided preference skeleton
    This patch makes it possible for distributions to have a global
    default preferences file in /etc/iridium-browser/preferences that get
    copied over to ~ when the user starts the program for the first time.

--- chrome/browser/first_run/first_run_internal_linux.cc
+++ chrome/browser/first_run/first_run_internal_linux.cc
@@ -21,9 +21,7 @@ bool IsOrganicFirstRun() {
 base::FilePath InitialPrefsPath() {
   // The standard location of the initial prefs is next to the chrome binary.
   base::FilePath initial_prefs;
-  if (!base::PathService::Get(base::DIR_EXE, &initial_prefs))
-    return base::FilePath();
-
+  initial_prefs = base::FilePath("/etc/iridium-browser");
   base::FilePath new_path = initial_prefs.AppendASCII(installer::kInitialPrefs);
   if (base::PathIsReadable(new_path))
     return new_path;

7ec5f5ab2ece21edbc260067778dedb690ade960
Jan Engelhardt <jengelh@inai.de>
2020-05-24 08:24:42 +0200

    prefs: use system-provided preference skeleton
    This patch makes it possible for distributions to have a global
    default preferences file in /etc/iridium-browser/preferences that get
    copied over to ~ when the user starts the program for the first time.

--- chrome/browser/extensions/api/management/management_browsertest.cc
+++ chrome/browser/extensions/api/management/management_browsertest.cc
@@ -586,7 +586,7 @@ namespace {
 const char kForceInstallNotEmptyHelp[] =
     "A policy may already be controlling the list of force-installed "
     "extensions. Please remove all policy settings from your computer "
-    "before running tests. E.g. from /etc/chromium/policies Linux or "
+    "before running tests. E.g. from /etc/iridium-browser/policies Linux or "
     "from the registry on Windows, etc.";
 
 }

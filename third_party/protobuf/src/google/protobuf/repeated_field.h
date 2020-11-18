54de0dfd098013a566b575041a214e712e858da0
Jan Engelhardt <jengelh@inai.de>
2020-11-18 12:30:34 +0100

    GFA chromium-78-protobuf-RepeatedPtrField-export.patch

--- third_party/protobuf/src/google/protobuf/repeated_field.h
+++ third_party/protobuf/src/google/protobuf/repeated_field.h
@@ -804,7 +804,7 @@ class StringTypeHandler {
 // RepeatedPtrField is like RepeatedField, but used for repeated strings or
 // Messages.
 template <typename Element>
-class RepeatedPtrField final : private internal::RepeatedPtrFieldBase {
+class PROTOBUF_EXPORT RepeatedPtrField final : private internal::RepeatedPtrFieldBase {
  public:
   RepeatedPtrField();
   explicit RepeatedPtrField(Arena* arena);

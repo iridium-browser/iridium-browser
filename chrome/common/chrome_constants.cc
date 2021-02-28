00516a7d634f011ebba6540efac7aa9d9fb03f15
Jan Engelhardt <jengelh@inai.de>
2021-02-28 14:58:18 +0100

    Set build name

--- chrome/common/chrome_constants.cc
+++ chrome/common/chrome_constants.cc
@@ -12,11 +12,11 @@
 
 #if BUILDFLAG(IS_MAC)
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-#define PRODUCT_STRING "Google Chrome"
+#define PRODUCT_STRING "Iridium"
 #elif BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
-#define PRODUCT_STRING "Google Chrome for Testing"
+#define PRODUCT_STRING "Still Iridium"
 #elif BUILDFLAG(CHROMIUM_BRANDING)
-#define PRODUCT_STRING "Chromium"
+#define PRODUCT_STRING "Iridium"
 #else
 #error Unknown branding
 #endif

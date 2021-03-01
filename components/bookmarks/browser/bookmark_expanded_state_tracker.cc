74b132903228037ac65bdca63495fb0b300dae75
Jan Engelhardt <jengelh@inai.de>
2021-03-01 10:13:47 +0100

    GFA chromium-gcc11.patch (partial)

--- components/bookmarks/browser/bookmark_expanded_state_tracker.cc
+++ components/bookmarks/browser/bookmark_expanded_state_tracker.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstddef>
 #include "components/bookmarks/browser/bookmark_expanded_state_tracker.h"
 
 #include <stdint.h>

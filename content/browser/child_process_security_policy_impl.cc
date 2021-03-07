285332d6bb5491ae65499a3560e232c4fc297448
Jan Engelhardt <jengelh@inai.de>
2021-03-07 22:42:50 +0100

    net: add "trk:" scheme and help identify URLs being retrieved
    The Chromium codebase has left us with a number of suspect URLs, and
    we want to know if the browser attempts to contact those sites.
    
    This patch introduces a new scheme, "trk:", which, when attempted to
    being processed, will dump a warning onto the screen as the resource
    is loaded. All URLs we think are suspect are "blacklisted" by
    prepending the new scheme to an existing URL:
    
    	trk:1234:https://clients4.google.com/
    	trk:https://clients4.google.com/ (unnumbered old variant)
    	trk:0.1234:https://... (stderr only, no UI reporting)
    
    Upon seeing a warning, we then know to investigate further, and either
    (a) whitelist the URL, that is, remove the trk: prefix and not show
    the warning, and/or (b) disable the particular feature which caused
    the loading of the URL in the first place, by default.
    
    Implementation:
    
    We hack up the URLFetcher class which sits in the network stack, and
    most of the URL that get loaded pass through here. The trk: prefix is
    stripped and processing continues with the inner URL.

--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -756,6 +756,7 @@ ChildProcessSecurityPolicyImpl::ChildProcessSecurityPolicyImpl()
   RegisterPseudoScheme(url::kJavaScriptScheme);
   RegisterPseudoScheme(kViewSourceScheme);
   RegisterPseudoScheme(kGoogleChromeScheme);
+  RegisterPseudoScheme(url::kTraceScheme);
 }
 
 ChildProcessSecurityPolicyImpl::~ChildProcessSecurityPolicyImpl() {
--- services/network/public/cpp/simple_url_loader.cc
+++ services/network/public/cpp/simple_url_loader.cc
@@ -39,6 +39,7 @@
 #include "net/base/request_priority.h"
 #include "net/http/http_request_headers.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "net/url_request/url_request.h"
 #include "services/network/public/cpp/data_element.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/simple_url_loader_stream_consumer.h"
@@ -1637,6 +1638,7 @@ void SimpleURLLoaderImpl::Start(mojom::URLLoaderFactory* url_loader_factory) {
   DCHECK(!url_loader_);
   DCHECK(!request_state_->body_started);
 
+  iridium::log_request("SimpleURLLoaderImpl", resource_request_->url);
   // Stash the information if retries are enabled or the request can be batched.
   if (remaining_retries_ > 0 || throttle_) {
     // Clone the URLLoaderFactory, to avoid any dependencies on its lifetime.

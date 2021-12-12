28d47fd21f395f385a75ac43a7ba5adfd74da630
Jan Engelhardt <jengelh@inai.de>
2021-12-12 02:11:56 +0100

    SUSE chromium-gcc11.patch

--- chrome/browser/ui/bookmarks/bookmark_tab_helper.cc
+++ chrome/browser/ui/bookmarks/bookmark_tab_helper.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstddef>
 #include "chrome/browser/ui/bookmarks/bookmark_tab_helper.h"
 
 #include "base/observer_list.h"

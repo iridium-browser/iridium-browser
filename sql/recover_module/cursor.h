0130d963e0d9678f694a013221d338c5f9f1ea68
Jan Engelhardt <jengelh@inai.de>
2021-12-21 16:46:16 +0100

    SUSE chromium-94-sql-no-assert.patch

--- sql/recover_module/cursor.h
+++ sql/recover_module/cursor.h
@@ -63,10 +63,12 @@ class VirtualCursor {
   // |sqlite_cursor| must have been returned by VirtualTable::SqliteCursor().
   static inline VirtualCursor* FromSqliteCursor(
       sqlite3_vtab_cursor* sqlite_cursor) {
+#ifndef NDEBUG
     static_assert(std::is_standard_layout<VirtualCursor>::value,
                   "needed for the reinterpret_cast below");
     static_assert(offsetof(VirtualCursor, sqlite_cursor_) == 0,
                   "sqlite_cursor_ must be the first member of the class");
+#endif
     VirtualCursor* result = reinterpret_cast<VirtualCursor*>(sqlite_cursor);
     DCHECK_EQ(sqlite_cursor, &result->sqlite_cursor_);
     return result;

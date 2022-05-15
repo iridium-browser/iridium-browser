b636d856dcf19d97e66758a6e94ba85f892f0431
Jan Engelhardt <jengelh@inai.de>
2022-05-15 15:02:41 +0200

    SUSE system-libdrm.patch

--- ui/ozone/platform/drm/gpu/crtc_controller.h
+++ ui/ozone/platform/drm/gpu/crtc_controller.h
@@ -11,7 +11,7 @@
 
 #include "base/memory/weak_ptr.h"
 #include "base/trace_event/traced_value.h"
-#include "third_party/libdrm/src/include/drm/drm_fourcc.h"
+#include <drm_fourcc.h>
 #include "ui/gfx/swap_result.h"
 #include "ui/ozone/platform/drm/common/scoped_drm_types.h"
 #include "ui/ozone/platform/drm/gpu/drm_overlay_plane.h"

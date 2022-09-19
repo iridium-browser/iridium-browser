73c53a9502b44fd937b3d8155b7eb7af543c5ee8
Jan Engelhardt <jengelh@inai.de>
2022-09-19 12:07:22 +0200

    SUSE chromium-105-wayland-1.20.patch

--- third_party/wayland/src/src/wayland-client-core.h
+++ third_party/wayland/src/src/wayland-client-core.h
@@ -119,8 +119,26 @@ struct wl_display;
  */
 struct wl_event_queue;
 
+/** Destroy proxy after marshalling
+ * @ingroup wl_proxy
+ */
+#define WL_MARSHAL_FLAG_DESTROY (1 << 0)
+
 void
 wl_event_queue_destroy(struct wl_event_queue *queue);
+
+struct wl_proxy *
+wl_proxy_marshal_flags(struct wl_proxy *proxy, uint32_t opcode,
+		       const struct wl_interface *interface,
+		       uint32_t version,
+		       uint32_t flags, ...);
+
+struct wl_proxy *
+wl_proxy_marshal_array_flags(struct wl_proxy *proxy, uint32_t opcode,
+			     const struct wl_interface *interface,
+			     uint32_t version,
+			     uint32_t flags,
+			     union wl_argument *args);
 
 void
 wl_proxy_marshal(struct wl_proxy *p, uint32_t opcode, ...);

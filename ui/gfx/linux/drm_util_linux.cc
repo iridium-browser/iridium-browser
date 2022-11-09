a7bbc1aa5b42ff363998fbc1ee9ae716111afeb1
Jan Engelhardt <jengelh@inai.de>
2022-11-09 19:06:51 +0100

    SUSE chromium-lp151-old-drm.patch

--- ui/gfx/linux/drm_util_linux.cc
+++ ui/gfx/linux/drm_util_linux.cc
@@ -6,6 +6,27 @@
 
 #include <drm_fourcc.h>
 
+#ifndef DRM_FORMAT_P010
+/*
+ * 2 plane YCbCr MSB aligned
+ * index 0 = Y plane, [15:0] Y:x [10:6] little endian
+ * index 1 = Cr:Cb plane, [31:0] Cr:x:Cb:x [10:6:10:6] little endian
+ */
+#define DRM_FORMAT_P010		fourcc_code('P', '0', '1', '0') /* 2x2 subsampled Cr:Cb plane 10 bits per channel */
+/*
+ * 2 plane YCbCr MSB aligned
+ * index 0 = Y plane, [15:0] Y:x [12:4] little endian
+ * index 1 = Cr:Cb plane, [31:0] Cr:x:Cb:x [12:4:12:4] little endian
+ */
+#define DRM_FORMAT_P012		fourcc_code('P', '0', '1', '2') /* 2x2 subsampled Cr:Cb plane 12 bits per channel */
+/*
+ * 2 plane YCbCr MSB aligned
+ * index 0 = Y plane, [15:0] Y little endian
+ * index 1 = Cr:Cb plane, [31:0] Cr:Cb [16:16] little endian
+ */
+#define DRM_FORMAT_P016		fourcc_code('P', '0', '1', '6') /* 2x2 subsampled Cr:Cb plane 16 bits per channel */
+#endif
+
 #include "base/notreached.h"
 
 namespace ui {

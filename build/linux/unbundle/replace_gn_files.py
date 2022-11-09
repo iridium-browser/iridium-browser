5840c108ea61f6678028228cd2d08297df0740fa
Jan Engelhardt <jengelh@inai.de>
2022-11-09 19:05:33 +0100

    SUSE chromium-system-libusb.patch

--- build/linux/unbundle/replace_gn_files.py
+++ build/linux/unbundle/replace_gn_files.py
@@ -53,6 +53,7 @@ REPLACEMENTS = {
   'libjpeg': 'third_party/libjpeg.gni',
   'libjxl' : 'third_party/libjxl/BUILD.gn',
   'libpng': 'third_party/libpng/BUILD.gn',
+  'libusb': 'third_party/libusb/BUILD.gn',
   'libvpx': 'third_party/libvpx/BUILD.gn',
   'libwebp': 'third_party/libwebp/BUILD.gn',
   'libxml': 'third_party/libxml/BUILD.gn',

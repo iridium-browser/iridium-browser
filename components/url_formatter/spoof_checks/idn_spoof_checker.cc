0d96934111cc97dbf07033d3bd500bfffabdb9e6
Jan Engelhardt <jengelh@inai.de>
2023-07-19 22:01:28 +0200

    SUSE chromium-icu72-2.patch

--- components/url_formatter/spoof_checks/idn_spoof_checker.cc
+++ components/url_formatter/spoof_checks/idn_spoof_checker.cc
@@ -737,6 +737,15 @@ void IDNSpoofChecker::SetAllowedUnicodeSet(UErrorCode* status) {
   allowed_set.remove(0x200Du);  // Zero Width Joiner
 #endif
 
+#if U_ICU_VERSION_MAJOR_NUM < 72
+  // Unicode 15 changes ZWJ and ZWNJ from allowed to restricted. Restrict them
+  // in lower versions too. This only relevant in Non-Transitional Mode as
+  // Transitional Mode maps these characters out.
+  // TODO(crbug.com/1386204): Remove these after ICU 72 is rolled out.
+  allowed_set.remove(0x200Cu);  // Zero Width Non-Joiner
+  allowed_set.remove(0x200Du);  // Zero Width Joiner
+#endif
+
   uspoof_setAllowedUnicodeSet(checker_, &allowed_set, status);
 }
 

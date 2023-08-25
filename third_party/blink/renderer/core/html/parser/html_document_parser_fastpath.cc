a869b819cd5828805bd50e2c079088e5a2101993
Jan Engelhardt <jengelh@inai.de>
2023-08-25 22:19:16 +0200

    SUSE chromium-115-lp155-typename.patch

--- third_party/blink/renderer/core/html/parser/html_document_parser_fastpath.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser_fastpath.cc
@@ -169,7 +169,7 @@ class HTMLFastPathParser {
   using Span = base::span<const Char>;
   using USpan = base::span<const UChar>;
   // 32 matches that used by HTMLToken::Attribute.
-  typedef std::conditional<std::is_same_v<Char, UChar>,
+  typedef typename std::conditional<std::is_same_v<Char, UChar>,
                            UCharLiteralBuffer<32>,
                            LCharLiteralBuffer<32>>::type LiteralBufferType;
   typedef UCharLiteralBuffer<32> UCharLiteralBufferType;

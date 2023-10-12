fb8a53d23f6e5863842b42eccac08e865b136744
Jan Engelhardt <jengelh@inai.de>
2023-10-12 09:30:13 +0200

    SUSE chromium-116-lp155-typenames.patch

--- third_party/blink/renderer/core/paint/object_paint_properties_sparse.h
+++ third_party/blink/renderer/core/paint/object_paint_properties_sparse.h
@@ -272,8 +272,8 @@ class CORE_EXPORT ObjectPaintPropertiesSparse : public ObjectPaintProperties {
       NodeList& nodes,
       NodeId node_id,
       const ParentType& parent,
-      NodeType::State&& state,
-      const NodeType::AnimationState& animation_state =
+      typename NodeType::State&& state,
+      const typename NodeType::AnimationState& animation_state =
           NodeType::AnimationState()) {
     // First, check if we need to add a new node.
     if (!nodes.HasField(node_id)) {
